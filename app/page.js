5190b90fc678f99ae23a62cecd2b270bd61f7bed
alpari01 <alan.parik456@gmail.com>
2023-08-29 14:08:52 +0300

    tailwind config, base setup

--- app/page.js
+++ app/page.js
@@ -1,95 +1,40 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="flex flex-col m-4 min-h-screen py-2 gap-4">
+        <h1 className="text-center bg-red-400">
+          Next.js Image: Optimizing Your Web Experience
+        </h1>
+        <h2 className="bg-blue-400">
+          Next.js Image improves modern web development by ballancing between
+          web performance and visual appeal. It enhances speed and visual
+          fidelity, making it an indispensable tool for crafting web experiences
+          that seamlessly blend efficiency and aesthetics.
+        </h2>
+        <div className="flex flex-col gap-4 bg-green-400">
+          <div>
+            <h3>
+              Next.js Image enhances web performance through several techniques.
+            </h3>
+            <h4>
+              It optimizes images by automatically selecting the appropriate
+              size and format based on the user's device, reducing data
+              transfer.
+            </h4>
+            <h4>
+              It employs lazy loading, loading images only as they enter the
+              viewport, which speeds up initial page load times.
+            </h4>
+            <h4>
+              Priority loading can be assigned to vital images, further
+              improving perceived performance by ensuring crucial content loads
+              swiftly.
+            </h4>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
