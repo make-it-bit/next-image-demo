19c612dd1f786d5debb080fd04d6ef19e07b0f15
alpari01 <alan.parik456@gmail.com>
2023-08-29 17:28:36 +0300

    update blog text content

--- app/page.js
+++ app/page.js
@@ -77,83 +77,179 @@ export default function Home() {
               </p>
             </h4>
           </div>
+          <div className="bg-gray-300 p-4">
+            <h3>
+              <b className="border-b-2 border-gray-800">"sizes" attribute</b>
+            </h3>
+            <h4>
+              The "sizes" attribute defines a set of media conditions (such as
+              screen width) and corresponding image source sizes. The format is
+              "media-condition-1 length-1, media-condition-2 length-2, ....""
+            </h4>
+            <h4>
+              <b>How It Works</b>
+              <p>
+                When a browser encounters an {'<Image>'} tag with the "srcset"
+                and "sizes" attributes, it uses the "sizes" attribute to
+                determine how much space the image will occupy within its
+                containing element at different screen widths.
+              </p>
+            </h4>
+            <h4>
+              <b>Why use "sizes"?</b>
+              <p>
+                By using the "sizes" attribute, you can ensure that the correct
+                image source is loaded for each device's screen size, optimizing
+                the performance and user experience of your responsive images.
+              </p>
+            </h4>
+          </div>
+          <div className="bg-gray-300 p-4">
+            <h3>
+              <b className="border-b-2 border-gray-800">
+                Lazy Loading vs. Priority Loading
+              </b>
+            </h3>
+            <h4>
+              <b>Lazy loading</b>
+              <p>
+                Use lazy loading for images that are not immediately visible on
+                the user's screen when the page loads. Lazy loading reduces
+                initial load times and saves bandwidth by loading images only
+                when they are needed.
+              </p>
+            </h4>
+            <h4>
+              <b>Priority loading</b>
+              <p>
+                Use priority loading for images that are crucial to the initial
+                user experience. This includes hero images, banners, or other
+                key visuals that define your website's identity. By using the
+                priority attribute with Next.js Image, you ensure that these
+                important images are loaded with higher priority.
+              </p>
+            </h4>
+          </div>
+          <div className="bg-gray-300 p-4">
+            <h3>
+              <b className="border-b-2 border-gray-800">Case studies</b>
+            </h3>
+            <h4>
+              <b>bitropia.com</b>
+              <p className="flex flex-col gap-4">
+                <Image
+                  src="/bitropia_lighthouse_scores.png"
+                  width={1000}
+                  height={384}
+                />
+                <Image src="/bitropia_blog.png" width={1000} height={384} />
+              </p>
+              <b>webtailor.ee</b>
+              <p>*add lighthouse scores here?*</p>
+            </h4>
+          </div>
         </div>
         <div className="bg-gray-300 p-4">
-          <h3>
-            <b className="border-b-2 border-gray-800">"sizes" attribute</b>
-          </h3>
-          <h4>
-            The "sizes" attribute defines a set of media conditions (such as
-            screen width) and corresponding image source sizes. The format is
-            "media-condition-1 length-1, media-condition-2 length-2, ....""
-          </h4>
-          <h4>
-            <b>How It Works</b>
-            <p>
-              When a browser encounters an {'<Image>'} tag with the "srcset" and
-              "sizes" attributes, it uses the "sizes" attribute to determine how
-              much space the image will occupy within its containing element at
-              different screen widths.
-            </p>
-          </h4>
-          <h4>
-            <b>Why use "sizes"?</b>
-            <p>
-              By using the "sizes" attribute, you can ensure that the correct
-              image source is loaded for each device's screen size, optimizing
-              the performance and user experience of your responsive images.
-            </p>
-          </h4>
-        </div>
-        <div className="bg-gray-300 p-4">
-          <h3>
-            <b className="border-b-2 border-gray-800">
-              Lazy Loading vs. Priority Loading
-            </b>
-          </h3>
-          <h4>
-            <b>Lazy loading</b>
-            <p>
-              Use lazy loading for images that are not immediately visible on
-              the user's screen when the page loads. Lazy loading reduces
-              initial load times and saves bandwidth by loading images only when
-              they are needed.
-            </p>
-          </h4>
-          <h4>
-            <b>Priority loading</b>
-            <p>
-              Use priority loading for images that are crucial to the initial
-              user experience. This includes hero images, banners, or other key
-              visuals that define your website's identity. By using the priority
-              attribute with Next.js Image, you ensure that these important
-              images are loaded with higher priority.
-            </p>
-          </h4>
-        </div>
-        <div className="bg-gray-300 p-4">
-          <h3>
-            <b className="border-b-2 border-gray-800">
-              Case studies and examples
-            </b>
-          </h3>
-          <h4>
-            <b>bitropia.com</b>
-            <p className="flex flex-col gap-4">
-              <Image
-                src="/bitropia_lighthouse_scores.png"
-                width={1000}
-                height={384}
-              />
-              <Image src="/bitropia_blog.png" width={1000} height={384} />
-            </p>
-            <b>webtailor.ee</b>
-            <p>*add lighthouse scores here?*</p>
-          </h4>
-          <h4>
-            <b>Bad image optimization</b>
-            <p>*show bad lightouse scores and long load times*</p>
-          </h4>
+          <b className="border-b-2 border-gray-800">
+            Image attributes examples
+          </b>
+          <div>
+            <div>
+              <p>
+                Test this sample image with different {'<Image>'} attributes.
+              </p>
+              <p>
+                Original image ratio is 600x400, format .jpg and size of 29KB
+              </p>
+              <Image src="/sample.jpg" width={600} height={400} />
+            </div>
+            <div className="mt-12">
+              <p>
+                Changing <b>width</b> and <b>height</b> properties
+              </p>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">
+                  width: 600; height: 400 (original)
+                </p>
+                <Image src="/sample.jpg" width={600} height={400} />
+              </div>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">
+                  width: 1200; height: 800 (two times bigger)
+                </p>
+                <Image src="/sample.jpg" width={1200} height={800} />
+              </div>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">
+                  width: 300; height: 200 (two times smaller)
+                </p>
+                <Image src="/sample.jpg" width={300} height={200} />
+              </div>
+            </div>
+            <div className="mt-12">
+              <p>
+                Setting incorrect ratio of <b>width</b> and <b>height</b>
+              </p>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">
+                  width: 400; height: 600 (original is 600x400)
+                </p>
+                <Image src="/sample.jpg" width={400} height={600} />
+              </div>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">width: 500; height: 10</p>
+                <Image src="/sample.jpg" width={500} height={10} />
+              </div>
+            </div>
+            <div className="mt-12">
+              <p>
+                Setting priority to <b>true</b> and <b>false</b>
+              </p>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">
+                  width: 600; height: 400, priority: true
+                </p>
+                <Image
+                  src="/sample.jpg"
+                  width={600}
+                  height={400}
+                  priority={true}
+                />
+              </div>
+              <div className="flex flex-col">
+                <p className="bg-gray-400 p-2 mt-6">
+                  width: 600; height: 400, priority: false
+                </p>
+                <Image
+                  src="/sample.jpg"
+                  width={600}
+                  height={400}
+                  priority={false}
+                />
+              </div>
+            </div>
+            <div className="mt-12">
+              <p>
+                Using <b>alt</b> property. Alt is shown when image fails to
+                load.
+              </p>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">no alt property</p>
+                <Image src="/wrong_path.jpg" width={600} height={400} />
+              </div>
+              <div>
+                <p className="bg-gray-400 p-2 mt-6">
+                  alt: "some alternative text"
+                </p>
+                <Image
+                  src="/wrong_path.jpg"
+                  width={600}
+                  height={400}
+                  alt={'some alternative text'}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
